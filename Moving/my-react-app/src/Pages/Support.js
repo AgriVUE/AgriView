732be3149207ea43bdb258cb0f35aa20e3b2a50e
TaiORashid <taizo.okra.rashid@gmail.com>
2024-10-06 23:25:33 -0400

    Added support working support feature

--- Moving/my-react-app/src/Pages/Support.js
+++ Moving/my-react-app/src/Pages/Support.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from '../Components/Container'; // Adjust the import path as necessary
+import ChatBox from '../Components/ChatBox'; // Import ChatBox component
+import '../ComponentCss/Support.css'; // Import the CSS file for styling
 
 const SupportPage = ({ setCurrentPage }) => {
   return (
-    <div>
-      <h1>Support Page</h1>
-      <button onClick={() => setCurrentPage('HomePage')}>Back to Home</button>
+    <div className="content">
+      <Container height={300} content={[
+        <ChatBox key="5" /> // Integrate the ChatBox here
+      ]} />
     </div>
   );
 };
